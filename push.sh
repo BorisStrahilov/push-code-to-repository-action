b96d2143807478e9e4a0882645efed1f304f60b7
Boris Strahilov <boris.strahilov@avionos.com>
2020-05-01 10:28:57 -0500

    More fixes to push

--- push.sh
+++ push.sh
@@ -3,6 +3,7 @@ set -eu
 
 git remote add mirror "https://$INPUT_REPOSITORY_USERNAME:$INPUT_REPOSITORY_PASSWORD@$INPUT_REPOSITORY_URL"
 
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done; git fetch --all; git pull --all
+git fetch --all
+git pull --all
 
 git push --all mirror
