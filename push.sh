23476e6d154fe694756cf03f68a8168895557d7a
Boris Strahilov <boris.strahilov@avionos.com>
2020-05-01 10:04:43 -0500

    added pulls from mirror remote

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-git remote add mirror "https://$INPUT_REPOSITORY_USERNAME:$INPUT_REPOSITORY_PASSWORD@$INPUT_REPOSITORY_URL"
 git fetch origin
 git pull origin
+git remote add mirror "https://$INPUT_REPOSITORY_USERNAME:$INPUT_REPOSITORY_PASSWORD@$INPUT_REPOSITORY_URL"
+git fetch mirror
+git pull mirror
 git push --all mirror
