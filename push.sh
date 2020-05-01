70bfab81056693ff804eb94bd7b7a5833d01c487
Boris Strahilov <boris.strahilov@avionos.com>
2020-05-01 09:57:58 -0500

    more updates to the push command.

--- push.sh
+++ push.sh
@@ -2,4 +2,6 @@
 set -eu
 
 git remote add mirror "https://$INPUT_REPOSITORY_USERNAME:$INPUT_REPOSITORY_PASSWORD@$INPUT_REPOSITORY_URL"
-git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
+git fetch origin
+git pull origin
+git push --all mirror
