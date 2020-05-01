22e27591cff02e61f4b2f61f06d3e80e10027305
Boris Strahilov <boris.strahilov@avionos.com>
2020-05-01 10:17:11 -0500

    now fetches & pulls from all remotes

--- push.sh
+++ push.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
-git fetch origin
-git pull origin
 git remote add mirror "https://$INPUT_REPOSITORY_USERNAME:$INPUT_REPOSITORY_PASSWORD@$INPUT_REPOSITORY_URL"
-git fetch mirror
-git pull mirror
+
+git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done; git fetch --all; git pull --all
+
 git push --all mirror
