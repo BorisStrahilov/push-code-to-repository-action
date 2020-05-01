3812a6f726042b01ca16c7a3fb0e4c0c84d36f32
Boris Strahilov <boris.strahilov@avionos.com>
2020-05-01 12:35:19 -0500

    no message

--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@
 set -eu
 
 git remote add mirror "https://$INPUT_REPOSITORY_USERNAME:$INPUT_REPOSITORY_PASSWORD@$INPUT_REPOSITORY_URL"
-git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
+git fetch origin --prune
+git push --prune mirror +refs/remotes/old-origin/*:refs/heads/* +refs/tags/*:refs/tags/*
